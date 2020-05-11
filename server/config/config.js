ba8436851265ba46f14f61267491e7d493d3062d
David Rios <davidsamuelrios02@gmail.com>
2020-05-10 20:23:28 -0400

    Fixed comments

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ process.env.PORT = process.env.PORT || 3000;
 // ENTORNO
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// BASE DE DATOS
+// Conexion - BASE DE DATOS
 let urlDB;
 
 if(process.env.NODE_ENV === 'dev') {
