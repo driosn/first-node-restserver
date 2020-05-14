70efc28c9d89e2212b2413abc0a4edc1232fe3f3
David Rios <davidsamuelrios02@gmail.com>
2020-05-13 23:51:11 -0400

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,16 @@ process.env.PORT = process.env.PORT || 3000;
 // ENTORNO
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// VENCIMIENTO DEL TOKEN
+// 60 segundos
+// 60 minutos 
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// SEED de autenticacion
+process.env.SEED_TOKEN = process.env.SEED_TOKEN  || 'este-es-el-seed-desarrollo';
+
 // Conexion - BASE DE DATOS
 let urlDB;
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,15 +5,17 @@ const _ = require('underscore');
 
 const app = express();
 
-app.get('/usuario', function(req, res) {
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion');
+
+app.get('/usuario', verificaToken, function(req, res) {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
 
-    let limite = req.query.limite || 5;
+    let limite = req.query.limite || 15;
     limite = Number(limite);
 
-    Usuario.find({ estado: true }, 'nombre email role estado google img')
+    Usuario.find({}, 'nombre email role estado google img')
             .skip(desde)
             .limit(limite)
             .exec((err, usuarios) => {
@@ -25,7 +27,7 @@ app.get('/usuario', function(req, res) {
                     });
                 }
 
-                Usuario.count({ estado: true }, (err, conteo) => {
+                Usuario.count({}, (err, conteo) => {
                     res.json({
                         ok: true,
                         usuarios,
@@ -37,7 +39,7 @@ app.get('/usuario', function(req, res) {
 
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], function(req, res) {
 
     let body = req.body;
 
@@ -65,7 +67,7 @@ app.post('/usuario', function(req, res) {
     });
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], function(req, res) {
     
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -87,7 +89,7 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], function(req, res) {
 
     let id = req.params.id;
 
